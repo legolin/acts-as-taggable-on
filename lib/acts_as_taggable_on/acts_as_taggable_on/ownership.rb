a01a70e21d8305df6e43f827913f2d2349b34c39
Adam Elhardt <adam@sapphireim.com>
2011-09-02 14:12:20 -0300

    owner_tags_on should reference the owner's base class to match how tagger_type is set by

--- lib/acts_as_taggable_on/acts_as_taggable_on/ownership.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/ownership.rb
@@ -35,7 +35,7 @@ module ActsAsTaggableOn::Taggable
         else
           base_tags.where([%(#{ActsAsTaggableOn::Tagging.table_name}.context = ? AND
                              #{ActsAsTaggableOn::Tagging.table_name}.tagger_id = ? AND
-                             #{ActsAsTaggableOn::Tagging.table_name}.tagger_type = ?), context.to_s, owner.id, owner.class.to_s]).all          
+                             #{ActsAsTaggableOn::Tagging.table_name}.tagger_type = ?), context.to_s, owner.id, owner.class.base_class.to_s]).all          
         end
       end
 
