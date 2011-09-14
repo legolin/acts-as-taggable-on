2b43e854395a31e96b0ae5d03cc95867611380cd
Adam Elhardt <adam@sapphireim.com>
2011-09-14 15:21:52 -0400

    Add :by => owner to tag searches to allow scoping tagged_with by owner

--- lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/core.rb
@@ -1,5 +1,5 @@
 module ActsAsTaggableOn::Taggable
-  module Core    
+  module Core
     def self.included(base)
       base.send :include, ActsAsTaggableOn::Taggable::Core::InstanceMethods
       base.extend ActsAsTaggableOn::Taggable::Core::ClassMethods
@@ -8,10 +8,10 @@ module ActsAsTaggableOn::Taggable
         attr_writer :custom_contexts
         after_save :save_tags
       end
-      
+
       base.initialize_acts_as_taggable_on_core
     end
-    
+
     module ClassMethods
       def initialize_acts_as_taggable_on_core
         tag_types.map(&:to_s).each do |tags_type|
@@ -38,14 +38,14 @@ module ActsAsTaggableOn::Taggable
               all_tags_list_on('#{tags_type}')
             end
           )
-        end        
+        end
       end
-      
+
       def acts_as_taggable_on(*args)
         super(*args)
         initialize_acts_as_taggable_on_core
       end
-      
+
       # all column names are necessary for PostgreSQL group clause
       def grouped_column_names_for(object)
         object.column_names.map { |column| "#{object.table_name}.#{column}" }.join(", ")
@@ -75,6 +75,8 @@ module ActsAsTaggableOn::Taggable
         conditions = []
 
         context = options.delete(:on)
+        tagger = options.delete(:by)
+
         alias_base_name = undecorated_table_name.gsub('.','_')
 
         if options.delete(:exclude)
@@ -89,13 +91,14 @@ module ActsAsTaggableOn::Taggable
           # setup taggings alias so we can chain, ex: items_locations_taggings_awesome_cool_123
           # avoid ambiguous column name
           taggings_context = context ? "_#{context}" : ''
-          
+
           #TODO: fix alias to be smaller
           taggings_alias   = "#{alias_base_name}#{taggings_context}_taggings_#{tags.map(&:safe_name).join('_')}_#{rand(1024)}"
 
           tagging_join  = "JOIN #{ActsAsTaggableOn::Tagging.table_name} #{taggings_alias}" +
                           "  ON #{taggings_alias}.taggable_id = #{table_name}.#{primary_key}" +
                           " AND #{taggings_alias}.taggable_type = #{quote_value(base_class.name)}"
+          tagging_join << " AND #{taggings_alias}.tagger_type = #{quote_value(tagger.class.base_class.name)} AND #{taggings_alias}.tagger_id = #{tagger.id}" if tagger
           tagging_join << " AND " + sanitize_sql(["#{taggings_alias}.context = ?", context.to_s]) if context
 
           # don't need to sanitize sql, map all ids and join with OR logic
@@ -117,6 +120,7 @@ module ActsAsTaggableOn::Taggable
                             "  ON #{taggings_alias}.taggable_id = #{table_name}.#{primary_key}" +
                             " AND #{taggings_alias}.taggable_type = #{quote_value(base_class.name)}" +
                             " AND #{taggings_alias}.tag_id = #{tag.id}"
+            tagging_join << " AND #{taggings_alias}.tagger_type = #{quote_value(tagger.class.base_class.name)} AND #{taggings_alias}.tagger_id = #{tagger.id}" if tagger
             tagging_join << " AND " + sanitize_sql(["#{taggings_alias}.context = ?", context.to_s]) if context
 
             joins << tagging_join
@@ -146,8 +150,8 @@ module ActsAsTaggableOn::Taggable
       def is_taggable?
         true
       end
-    end    
-    
+    end
+
     module InstanceMethods
       # all column names are necessary for PostgreSQL group clause
       def grouped_column_names_for(object)
@@ -200,7 +204,7 @@ module ActsAsTaggableOn::Taggable
 
         opts  =  ["#{tagging_table_name}.context = ?", context.to_s]
         scope = base_tags.where(opts)
-        
+
         if ActsAsTaggableOn::Tag.using_postgresql?
           group_columns = grouped_column_names_for(ActsAsTaggableOn::Tag)
           scope = scope.order("max(#{tagging_table_name}.created_at)").group(group_columns)
@@ -233,7 +237,7 @@ module ActsAsTaggableOn::Taggable
           instance_variable_set("@#{context.to_s.singularize}_list", nil)
           instance_variable_set("@all_#{context.to_s.singularize}_list", nil)
         end
-      
+
         super(*args)
       end
 
@@ -249,7 +253,7 @@ module ActsAsTaggableOn::Taggable
           current_tags = tags_on(context)
           old_tags     = current_tags - tag_list
           new_tags     = tag_list     - current_tags
-          
+
           # Find taggings to remove:
           old_taggings = taggings.where(:tagger_type => nil, :tagger_id => nil,
                                         :context => context.to_s, :tag_id => old_tags).all
